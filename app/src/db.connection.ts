4268b0fd517d25eadf4a1af1f539bae080197397
Atef Haque <atefth@gmail.com>
2022-04-01 20:10:42 +0100

    base app

---
+++ app/src/db.connection.ts
@@ -0,0 +1,17 @@
+import mysql from "mysql2";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const port = process.env.MYSQL_LOCAL_PORT || "3306";
+
+// Create the connection pool. The pool-specific settings are the defaults
+export const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: Number(port),
+  user: process.env.DB_USER,
+  password: process.env.DB_PWD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
