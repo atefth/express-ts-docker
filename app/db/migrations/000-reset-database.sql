f324fdaac3bf7266f01cbea8d1f4f92a51aa7019
Atef Haque <atefth@gmail.com>
2022-04-03 13:25:30 +0100

    fix db rest script

--- app/db/migrations/000-reset-database.sql
+++ app/db/migrations/000-reset-database.sql
@@ -1,3 +1,5 @@
 drop database if exists task_management_test;
 create database task_management_test;
-create database if not exists task_management;
+drop database if exists task_management_dev;
+create database task_management_dev;
+create database if not exists task_management_prod;
